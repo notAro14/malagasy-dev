06602e8a2c59c37e0b6042d834316dee65ba61f1
Aro Andriamaro <ni.aro.aina@gmail.com>
2020-06-02 21:55:22 +0200

    test formulaire netlify 2

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -31,7 +31,14 @@ class ContactForm extends React.Component {
   render() {
     const { name, email, message } = this.state
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={this.handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
             Your Name:{" "}
