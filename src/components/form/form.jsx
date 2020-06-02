ec746608f8c33bb62d0c614b339aac6822f48452
Aro Andriamaro <ni.aro.aina@gmail.com>
2020-06-02 22:16:06 +0200

    Implementation definitive du formulaire netlify avec material ui

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,4 +1,7 @@
 import React from "react"
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
+import styled from "styled-components"
 
 const encode = data => {
   return Object.keys(data)
@@ -6,10 +9,17 @@ const encode = data => {
     .join("&")
 }
 
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 70vh;
+`
+
 class ContactForm extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { name: "", email: "", message: "" }
+    this.state = { name: "", email: "", message: "", objet: "" }
   }
 
   /* Here’s the juicy bit for posting the form submission */
@@ -20,7 +30,10 @@ class ContactForm extends React.Component {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state }),
     })
-      .then(() => alert("Success!"))
+      .then(() => {
+        this.setState({ name: "", email: "", message: "", objet: "" })
+        alert("Message envoyé avec succès!")
+      })
       .catch(error => alert(error))
 
     e.preventDefault()
@@ -29,9 +42,9 @@ class ContactForm extends React.Component {
   handleChange = e => this.setState({ [e.target.name]: e.target.value })
 
   render() {
-    const { name, email, message } = this.state
+    // const { name, email, message, objet } = this.state
     return (
-      <form
+      <FormContainer
         name="contact"
         method="post"
         data-netlify="true"
@@ -39,42 +52,46 @@ class ContactForm extends React.Component {
         onSubmit={this.handleSubmit}
       >
         <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>
-            Your Name:{" "}
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email:{" "}
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:{" "}
-            <textarea
-              name="message"
-              value={message}
-              onChange={this.handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+        <TextField
+          onChange={this.handleChange}
+          required
+          type="text"
+          name="name"
+          label="nom"
+          id="name"
+        />
+        <TextField
+          onChange={this.handleChange}
+          required
+          type="email"
+          name="email"
+          label="email"
+          id="email"
+        />
+        <TextField
+          onChange={this.handleChange}
+          required
+          type="text"
+          name="objet"
+          label="objet"
+          id="objet"
+        />
+        <TextField
+          onChange={this.handleChange}
+          required
+          multiline
+          rows={6}
+          rowsMax={8}
+          variant="outlined"
+          type="text"
+          name="message"
+          label="message"
+          id="message"
+        />
+        <Button type="submit" variant="contained">
+          Envoyer
+        </Button>
+      </FormContainer>
     )
   }
 }
