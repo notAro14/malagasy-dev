3501f0612374e263853a773d67060ab9494d6a2f
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-03-08 20:50:58 +0100

    Add hard refresh option when new version of the site is published
    Cf https://github.com/gatsbyjs/gatsby/issues/9087#issuecomment-506136336

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,13 @@
 // import 'prismjs/themes/prism-funky.css'
 import 'prismjs/themes/prism-okaidia.css'
 import './src/font.css'
+
+exports.onServiceWorkerUpdateReady = () => {
+  if (
+    window.confirm(
+      'Il semblerait que ton navigateur ait gardé une ancienne version du site. Veux-tu mettre à jour maintenant ?'
+    )
+  ) {
+    window.location.reload(true)
+  }
+}
